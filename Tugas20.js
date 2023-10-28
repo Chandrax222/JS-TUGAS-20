870f8fbc78dd90f649db3a3fd045c245f84bc6f8
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-28 10:33:47 +0700

    Match
    Match

---
+++ Tugas20.js
@@ -0,0 +1,8 @@
+function Math() {
+    var isi = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+
+    console.log(isi.match(/are/));
+    console.log(" ");
+    console.log(isi.match(/words/));
+}
+Math();
